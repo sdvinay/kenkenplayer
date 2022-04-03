84db19e6c5f99625a22288d359c74c9606a02eaf
Vinay Kumar <sdvinay@gmail.com>
2022-04-02 17:00:51 -0700

    Consistently use Cage terminology

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ from collections import namedtuple
 import enum
 
 # Classes used for declaring the board
-Cage = namedtuple("Cage", "points op val")
+Cage = namedtuple("Cage", "cells op val")
 
 # The values correspond to those used in the encoded layout of the game
 class op(enum.Enum):
