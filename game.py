f7c4a46a1b9ea0f4847d14f07b30a773d9334491
Vinay Kumar <sdvinay@gmail.com>
2022-04-02 17:30:54 -0700

    Standardize on cell terminology, use stronger typing and validation

--- game.py
+++ game.py
@@ -2,9 +2,11 @@ from collections import namedtuple
 import enum
 
 # Classes used for declaring the board
+Cell = namedtuple("Cell", "row col")
 Cage = namedtuple("Cage", "cells op val")
+Puzzle = namedtuple("Puzzle", "board_size cages")
 
-# The values correspond to those used in the encoded layout of the game
+# The values for the ops correspond to those used in the encoded layout of the game
 class op(enum.Enum):
     DIV = '/'
     PROD = '*'
--- game_parser.py
+++ game_parser.py
@@ -5,12 +5,14 @@ import json
 from collections import namedtuple
 import enum
 
-from game import Cage, op
+from game import Cage, Cell, op, Puzzle
 
-def get_puzzle(puzzle_id):
+def get_puzzle(puzzle_id: int) -> Puzzle:
     return parse_puzzle_data(download_puzzle(puzzle_id))
 
-def download_puzzle(puzzle_id):
+PuzzleData = list[list[str]]
+
+def download_puzzle(puzzle_id: int) -> PuzzleData:
     req = requests.post('http://www.kenkenpuzzle.com/find_puzzle', {'puzzle_id': puzzle_id})
     if req.status_code != 200:
         raise RuntimeException(f'http error {req.status_code}')
@@ -24,51 +26,50 @@ def download_puzzle(puzzle_id):
     puzzle_data = [l.rstrip().split() for l in json.loads(puzzle_raw)['data'].split('\n')]
     return puzzle_data
 
-def parse_puzzle_data(puzzle_data):
+def parse_puzzle_data(puzzle_data: PuzzleData) -> Puzzle:
     board_size = puzzle_data.index(['T'])-1
 
     val_grid = puzzle_data[puzzle_data.index(['T'])+1:puzzle_data.index(['S'])]
     op_grid = puzzle_data[puzzle_data.index(['S'])+1:puzzle_data.index(['V'])]
     borders  = {'V': puzzle_data[puzzle_data.index(['V'])+1:puzzle_data.index(['H'])], 
         'H': puzzle_data[puzzle_data.index(['H'])+1:puzzle_data.index(['H'])+board_size+1]}
-    ops = {(row, col): op_grid[row][col] for row in range(board_size) for col in range(board_size) if op_grid[row][col] != '0'}
-    cages = [Cage( find_cage(pt, board_size, borders), op(opstr), int(val_grid[pt[0]][pt[1]]) ) for (pt, opstr) in ops.items()]
+    ops = {Cell(row, col): op_grid[row][col] for row in range(board_size) for col in range(board_size) if op_grid[row][col] != '0'}
+    cages = [Cage( find_cage(cell, board_size, borders), op(opstr), int(val_grid[cell.row][cell.col]) ) for (cell, opstr) in ops.items()]
 
-    return {'board_size': board_size, 'cages': cages}
+    return Puzzle(board_size, cages)
 
-def find_neighbors(cur, cage, board_size, borders):
+def find_neighbors(cell: Cell, cage: list[Cell], board_size: int, borders: dict) -> list[Cell]:
     to_search = []
 
     # try going down
-    if cur[0] < board_size-1:
-        if borders['H'][cur[1]][cur[0]] == '0':
-            next = (cur[0]+1, cur[1])
+    if cell.row < board_size-1:
+        if borders['H'][cell.col][cell.row] == '0':
+            next = Cell(cell.row+1, cell.col)
             if next not in cage:
                 cage.append(next)
                 to_search.append(next)
 
     # try going right
-    if cur[1] < board_size-1:
-        if borders['V'][cur[0]][cur[1]] == '0':
-            next = (cur[0], cur[1]+1)
+    if cell.col < board_size-1:
+        if borders['V'][cell.row][cell.col] == '0':
+            next = Cell(cell.row, cell.col+1)
             if next not in cage:
                 cage.append(next)
                 to_search.append(next)
 
     # try going left
-    if cur[1]>0:
-        if borders['V'][cur[0]][cur[1]-1] == '0':
-            next = (cur[0], cur[1]-1)
+    if cell.col>0:
+        if borders['V'][cell.row][cell.col-1] == '0':
+            next = Cell(cell.row, cell.col-1)
             if next not in cage:
                 cage.append(next)
                 to_search.append(next)
     return to_search
 
 
-def find_cage(starting_pt, board_size, borders):
-    cur = starting_pt
-    cage = [cur]
-    neighbors = find_neighbors(cur, cage, board_size, borders)
+def find_cage(start_cell: Cell, board_size: int, borders: dict) -> list[Cell]:
+    cage = [start_cell]
+    neighbors = find_neighbors(start_cell, cage, board_size, borders)
     if neighbors:
         for neighbor in neighbors:
             find_neighbors(neighbor, cage, board_size, borders)
